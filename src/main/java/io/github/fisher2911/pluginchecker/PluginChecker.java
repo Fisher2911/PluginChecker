51e6d848abb4d22dba2e230686b0f803cdcb5705
HeroBrineGoat <76707404+MasterOfTheFish@users.noreply.github.com>
2021-11-13 11:30:33 -0500

    Initial commit

---
+++ src/main/java/io/github/fisher2911/pluginchecker/PluginChecker.java
@@ -0,0 +1,58 @@
+package io.github.fisher2911.pluginchecker;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public final class PluginChecker extends JavaPlugin {
+
+    private String errorMessage = "";
+
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+        final FileConfiguration config = this.getConfig();
+        this.errorMessage = config.getString("not-installed-message");
+
+        final ConfigurationSection pluginSection = config.getConfigurationSection("required-plugins");
+
+        if (pluginSection == null) {
+            return;
+        }
+
+
+        final PluginManager pluginManager = this.getServer().getPluginManager();
+        final Logger logger = this.getLogger();
+
+        Bukkit.getScheduler().runTaskLater(this,
+                () -> {
+                    boolean disable = false;
+                    for (final String plugin : pluginSection.getKeys(false)) {
+                        final Plugin check = pluginManager.getPlugin(plugin);
+
+                        if (check == null) {
+                            logger.severe(errorMessage.replace("%plugin%", plugin));
+                            disable = disable || pluginSection.getBoolean(plugin);
+
+                        }
+                    }
+
+                    if (disable) {
+                        Bukkit.shutdown();
+                    }
+                },
+                20);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
